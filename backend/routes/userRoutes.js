2dc1d66276caf7eba97973041c7c05edf2c54ee5
Andrey <natsaievv@gmail.com>
2025-01-30 06:55:42 +0200

    added such function: search User

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {getProfile, updateProfile} from "../controllers/userController.js";
 import {createPost, getUserPosts, updatePost, deletePost} from "../controllers/postController.js";
-
+import {getUserByUsername} from "../controllers/searchUserController.js";
 
 const router = express.Router();
 
@@ -10,10 +10,16 @@ const router = express.Router();
 router.get("/profile", getProfile);
 router.put("/profile", updateProfile);
 
+
+// Роуты для работы с постами
 router.post("/create", createPost);
 router.get("/posts", getUserPosts);
 router.put("/posts/:id", updatePost);
 router.delete("/posts/:id", deletePost);
+
+
+// Роут для поиска пользователя
+router.get("/search/:username", getUserByUsername);
 
 // router.get('/profile', (req, res) => {
 //
